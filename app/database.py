1e7cbab939b88f3a3d371562379ff188503e3a6a
Rai-shwith <ashwithrai96.96@gmail.com>
2024-06-08 12:47:21 +0530

    Added Phone Number to user TABLE

--- app/database.py
+++ app/database.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 
-SQL_ALCHEMY_DATABASE_URL ="postgresql://postgres:109737@localhost/postgres"
-# SQL_ALCHEMY_DATABASE_URL =f"{settings.database_protocol}://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}?{settings.database_connection_parameter}"
+# SQL_ALCHEMY_DATABASE_URL ="postgresql://postgres:109737@localhost/postgres"
+SQL_ALCHEMY_DATABASE_URL =f"{settings.database_protocol}://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}?{settings.database_connection_parameter}"
 # use below line to connect to cocroachdb without certificate
 # SQL_ALCHEMY_DATABASE_URL =f"{settings.database_protocol}://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}?sslmode=require"
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
