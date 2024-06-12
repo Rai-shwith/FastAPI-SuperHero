e3137f63bff14d3cf92c1aebae1a1b00f7e2a028
Rai-shwith <ashwith96.96@gmail.com>
2024-06-12 15:21:24 +0530

    removed ssl certificate

--- app/database.py
+++ app/database.py
@@ -5,9 +5,9 @@ from .config import settings
 
 
 # SQL_ALCHEMY_DATABASE_URL ="postgresql://postgres:109737@localhost/postgres"
-SQL_ALCHEMY_DATABASE_URL =f"{settings.database_protocol}://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}?{settings.database_connection_parameter}"
+# SQL_ALCHEMY_DATABASE_URL =f"{settings.database_protocol}://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}?{settings.database_connection_parameter}"
 # use below line to connect to cocroachdb without certificate
-# SQL_ALCHEMY_DATABASE_URL =f"{settings.database_protocol}://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}?sslmode=require"
+SQL_ALCHEMY_DATABASE_URL =f"{settings.database_protocol}://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}?sslmode=require"
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 sessionlocal=sessionmaker(autoflush=False,bind=engine)
 
