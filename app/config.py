2c1d78f90fa260b4df2871533ea2f8010a987419
Rai-shwith <ashwith96.96@gmail.com>
2024-06-12 11:59:33 +0530

    modified .gitignore and added config.py

---
+++ app/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    database_protocol : str
+    database_username : str
+    database_password : str
+    database_host : str
+    database_port : str
+    database_name : str
+    database_connection_parameter : str
+    secret_key : str
+    algorithm : str
+    acess_token_expire_time : int
+
+    class config:
+        env_file = ".env"
+
+settings = Settings()
