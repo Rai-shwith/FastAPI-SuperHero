827643fcd3e14ca904e90a7e3bd4e2f3bc55fe6b
Rai-shwith <ashwith96.96@gmail.com>
2024-06-22 13:05:42 +0530

    authentication error handling in nav of all users page

--- app/templates/static/JS/simple/script.js
+++ app/templates/static/JS/simple/script.js
@@ -5,6 +5,22 @@ document.getElementById('myHeros').addEventListener('click', ev =>{
             'Authorization': `Bearer ${localStorage.token}`
     }}).then(response =>{
         if (!response.ok){
+            if (response.status == 401){
+                console.error(response.statusText)
+                const message = document.getElementById('center');
+                message.style.display='block';
+                document.getElementById('page-body').classList.add('body-opacity');
+                setTimeout(() => {
+                    window.location.href="/login"
+                    setTimeout(() => {
+                        message.style.display='none';
+                        document.getElementById('page-body').classList.remove('body-opacity');
+                        
+                    }, 50);
+                }, 1500);
+                
+
+            }
             throw new Error(response.statusText);
         }
         return response.json();
