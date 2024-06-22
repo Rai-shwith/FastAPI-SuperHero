34f1f1f92ca73e938c1369ff512bb4a27f88cfe5
Rai-shwith <ashwith96.96@gmail.com>
2024-06-22 12:23:32 +0530

    Added authorized error handling in home using js i made users to go to login page

--- app/templates/static/JS/simple/script.js
+++ app/templates/static/JS/simple/script.js
@@ -1,4 +1,3 @@
-console.log("hello")
 document.getElementById('myHeros').addEventListener('click', ev =>{
     fetch('/users/id',{
         method: 'GET',
@@ -14,7 +13,6 @@ document.getElementById('myHeros').addEventListener('click', ev =>{
         window.location.href=`/users/posts/${id}`;
     })
 })
-console.log("hello")
 document.getElementById('profile').addEventListener('click',ev =>{
     fetch('/users/id',{
         method: 'GET',
@@ -22,6 +20,22 @@ document.getElementById('profile').addEventListener('click',ev =>{
             'Authorization': `Bearer ${localStorage.token}`
     }}).then(response =>{
         if (!response.ok){
+            if (response.status == 401){
+                console.error(response.statusText)
+                const message = document.getElementById('center');
+                message.style.display='block';
+                document.getElementById('page-body').classList.add('body-opacity');
+                setTimeout(() => {
+                    window.location.href="/login"
+                    setTimeout(() => {
+                        message.style.display='none';
+                        document.getElementById('page-body').classList.remove('body-opacity');
+                        
+                    }, 50);
+                }, 1500);
+                
+
+            }
             throw new Error(response.statusText);
         }
         return response.json();
