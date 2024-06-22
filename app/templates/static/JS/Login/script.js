551d270efa5df147d006f71d54fa2d90fe6aa1ac
Rai-shwith <ashwith96.96@gmail.com>
2024-06-22 13:57:59 +0530

    Login Invalid credential error handled and succesfully logged in feature added

--- app/templates/static/JS/Login/script.js
+++ app/templates/static/JS/Login/script.js
@@ -13,14 +13,46 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
     })
         .then(response => {
             if (!response.ok) {
-                throw new Error(response.statusText);
+                if (response.status== 403){
+                    console.error(response.statusText)
+                    const message = document.getElementById('center');
+                    message.style.display='block';
+                    document.getElementById('page-body').classList.add('body-opacity');
+                    setTimeout(() => {
+    
+                            message.style.display='none';
+                            document.getElementById('page-body').classList.remove('body-opacity');
+                            
+                    }, 1500);
+                    
+                }
+               
+                throw new Error("Error while sending info: ",response.statusText)
             }
             return response.json();
         })
         .then(data => {
-            localStorage.setItem('token', data.acess_token);
-            localStorage.setItem('tokenType', data.token_type);
-            window.location.href = "/posts"
+                localStorage.setItem('token', data.acess_token);
+                localStorage.setItem('tokenType', data.token_type);
+                const message = document.getElementById('center');
+                message.innerHTML='Successfully Logged in'
+                message.style.color='green'
+                message.style.display='block';
+                document.getElementById('page-body').classList.add('body-opacity');
+                setTimeout(() => {
+                    document.getElementById('loginEmail').value = '';
+                    document.getElementById('loginPassword').value = '';
+                        window.location.href = "/posts"
+                        setTimeout(() => {
+                            message.style.display='none';
+                        message.innerHTML='Invalid Credentials'
+                        document.getElementById('page-body').classList.remove('body-opacity');
+                        }, 50);
+                        
+                        
+                }, 1500);
+                
+
         }).catch(error => {
             console.error('Error while Logging in: ', error)
         })
