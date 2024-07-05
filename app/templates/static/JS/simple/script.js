3418eb8dde5c02fc358cc98e3c9125b2758861ce
Rai-shwith <ashwith96.96@gmail.com>
2024-07-05 10:23:04 +0530

    Made delete functional There are works pending like i didnot made any confirmation

--- app/templates/static/JS/simple/script.js
+++ app/templates/static/JS/simple/script.js
@@ -1,63 +1,65 @@
-document.getElementById('myHeros').addEventListener('click', ev =>{
-    fetch('/users/id',{
+document.getElementById('myHeros').addEventListener('click', ev => {
+    fetch('/users/id', {
         method: 'GET',
         headers: {
             'Authorization': `Bearer ${localStorage.token}`
-    }}).then(response =>{
-        if (!response.ok){
-            if (response.status == 401){
+        }
+    }).then(response => {
+        if (!response.ok) {
+            if (response.status == 401) {
                 console.error(response.statusText)
                 const message = document.getElementById('center');
-                message.style.display='block';
+                message.style.display = 'block';
                 document.getElementById('page-body').classList.add('body-opacity');
                 setTimeout(() => {
-                    window.location.href="/login"
+                    window.location.href = "/login"
                     setTimeout(() => {
-                        message.style.display='none';
+                        message.style.display = 'none';
                         document.getElementById('page-body').classList.remove('body-opacity');
-                        
+
                     }, 50);
                 }, 1500);
-                
+
 
             }
             throw new Error(response.statusText);
         }
         return response.json();
-    }).then(userId=>{
+    }).then(userId => {
         id = userId.id;
-        window.location.href=`/users/posts/${id}`;
+        window.location.href = `/users/posts/${id}`;
     })
 })
-document.getElementById('profile').addEventListener('click',ev =>{
-    fetch('/users/id',{
+document.getElementById('profile').addEventListener('click', ev => {
+    fetch('/users/id', {
         method: 'GET',
         headers: {
             'Authorization': `Bearer ${localStorage.token}`
-    }}).then(response =>{
-        if (!response.ok){
-            if (response.status == 401){
+        }
+    }).then(response => {
+        if (!response.ok) {
+            if (response.status == 401) {
                 console.error(response.statusText)
                 const message = document.getElementById('center');
-                message.style.display='block';
+                message.style.display = 'block';
                 document.getElementById('page-body').classList.add('body-opacity');
                 setTimeout(() => {
-                    window.location.href="/login"
+                    window.location.href = "/login"
                     setTimeout(() => {
-                        message.style.display='none';
+                        message.style.display = 'none';
                         document.getElementById('page-body').classList.remove('body-opacity');
-                        
+
                     }, 50);
                 }, 1500);
-                
+
 
             }
             throw new Error(response.statusText);
         }
         return response.json();
-    }).then(userId=>{
+    }).then(userId => {
         id = userId.id;
-        window.location.href=`/users/${id}`;
+        window.location.href = `/users/${id}`;
     })
 })
 
@@ -66,7 +68,7 @@ document.getElementById('profile').addEventListener('click',ev =>{
 // })
 
 function toggleHeart(id) {
-    const src ='like-img'+id
+    const src = 'like-img' + id
     console.log(src)
     const img = document.getElementById(src);
     if (img.src.endsWith('blankheart.png')) {
@@ -75,4 +77,20 @@ function toggleHeart(id) {
     } else {
         img.src = '../static/images/blankheart.png'; // Change to empty heart image
     }
+}
+
+
+function deleteAccount() {
+    id = window.location.pathname.substring(7)//THis is because I cannot transfer large int from html so iam using id from the endpoint
+    fetch(`/users/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${localStorage.token}`
+        }
+    }).then(response => {
+        if (response.status == 204) {
+            localStorage.clear()
+            console.log("Account Deleted")
+            window.location.href = "/login"
+        }})
 }
